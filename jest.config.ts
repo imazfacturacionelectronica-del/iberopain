146e4ea080cb7a3d7ec5379d8adf32bfdf5171aa
lcima <imaz.facturacionelectronica@gmail.com>
2026-04-23 18:00:14 -0500

    fix: downgrade jest to 29.x for ts-jest compatibility, add testMatch

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
   testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/*.{spec,test}.{ts,tsx}'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
